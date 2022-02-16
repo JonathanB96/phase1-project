25224f1e49eb8c2526353bd396865e03b76d7414
jonathan bola <bolajonathan@yahoo.com>
2022-02-16 15:07:01 -0600

    global variables

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     
+    // Variables selected from HTML
+    let div = document.querySelector(".img-container");
+    let card = document.querySelector(".card");
+    let select = document.querySelector('#filter');
+    let btn1 = document.querySelector('#btn1'); 
+    let titleForm = document.querySelector('#title-form')
+
+    // Variables(created elements)
+    let h2 = document.createElement("h2");
+    let p = document.createElement("p");
+    let btn = document.createElement("button");
+    btn.textContent = "Like";
+    let img = document.createElement("img");
+
+
 
 
 
