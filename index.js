413d55891a5a1b097f4ca9c35003969bca41a77d
jonathan bola <bolajonathan@yahoo.com>
2022-02-16 15:17:47 -0600

    get request & display

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let card = document.querySelector(".card");
     let select = document.querySelector('#filter');
     let btn1 = document.querySelector('#btn1'); 
-    let titleForm = document.querySelector('#title-form')
+    let titleForm = document.querySelector('#title-form');
 
     // Variables(created elements)
     let h2 = document.createElement("h2");
@@ -13,6 +13,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let btn = document.createElement("button");
     btn.textContent = "Like";
     let img = document.createElement("img");
+    // API KEY
+    let apiKey = '563492ad6f91700001000001918af55e4f3f487ca84d981324668b46';
+
+    //Fetching photos from  the pexel API    
+    fetch("https://api.pexels.com/v1/curated", {
+      method: 'GET',
+      headers: {Accept: 'application/json',
+      Authorization: apiKey}
+    })
+    .then(res=>res.json())
+    .then(data =>{
+
+      //Displaying a random element of the array
+      let i = Math.floor(Math.random()*15) 
+      let arr = data.photos 
+      img.src = arr[i].src.original 
+      h2.textContent = 'Title'
+      p.textContent = "0"
+      //Rendering elements on the page
+      card.appendChild(img); 
+      card.appendChild(p);
+      card.appendChild(btn)
+      div.appendChild(card) 
+          
+      
+    })
+    
+
 
 
 
